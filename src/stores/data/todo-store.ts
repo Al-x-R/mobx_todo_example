5e73e3ee8a3a6bb56cd471b83f79c7c28642dfca
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-04-02 15:10:03 +0300

    Add todo list actions example

--- src/stores/data/todo-store.ts
+++ src/stores/data/todo-store.ts
@@ -1,4 +1,4 @@
-import {observable} from 'mobx'
+import {action, observable} from 'mobx'
 import RootStore from "../root-store";
 
 class Todo {
@@ -7,11 +7,38 @@ class Todo {
 
 export default class TodoStore {
     @observable
-    list: Todo[] = []
+    todoList: Todo[] = []
 
     private rootStore: RootStore;
 
     constructor(rootStore: RootStore) {
         this.rootStore = rootStore
     }
+
+    @action
+    addTodo(name: string, userId: number) {
+        this.todoList.push(new Todo(name, userId))
+    }
+
+    @action
+    getTodo(name: string) {
+        return this.todoList.find(todo => todo.name === name)
+    }
+
+    @action
+    getUserTodos(userId: number) {
+        return this.todoList.filter(todo => todo.userId === userId)
+    }
+
+    @action
+    removeTodo(name: string) {
+        const todoToRemove = this.getTodo(name)
+
+        if (todoToRemove) {
+            // todoToRemove.disporse()
+            const todoIndex = this.todoList.indexOf(todoToRemove)
+            this.todoList.splice(todoIndex, 1)
+        }
+    }
+
 }
--- src/stores/data/user-store.ts
+++ src/stores/data/user-store.ts
@@ -1,18 +1,52 @@
-import {observable} from 'mobx'
+import {action, computed, observable} from 'mobx'
 import RootStore from "../root-store";
 
-class User {
+let runningId = 0
 
+class User {
+    id: number = runningId++
+    private rootStore: RootStore;
+    @observable
+    name: string = ''
+
+    constructor(name: string, rootStore: RootStore) {
+        this.name = name
+        this.rootStore = rootStore
+    }
+
+    @computed
+    get todos() {
+        this.rootStore.dataStore.todoStore.getUserTodos(this.id)
+    }
 }
 
 export default class UserStore {
     @observable
-    list: User[] = []
+    usersList: User[] = []
 
     private rootStore: RootStore;
 
     constructor(rootStore: RootStore) {
         this.rootStore = rootStore
 
+    }
+
+    @action
+    addUser(name: string) {
+        this.usersList.push(new User(name, this.rootStore))
+    }
+
+    @action
+    getUser(name: string) {
+        return this.usersList.find(user => user.name === name)
+    }
+
+    @action
+    removeUser(name: string) {
+        const user = this.getUser(name)
+
+        if (user) {
+            user.todos.forEach(todo => this.rootStore.dataStore.todoStore.removeTodo(todo.name))
+        }
     }
 }
