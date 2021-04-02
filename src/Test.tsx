d7b17ee12307801b1030e18b4a2023999109bc09
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-04-02 16:10:22 +0300

    UseObserver for component

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+// import Todo from './stores/data/todos/todo'
+// import TodoStore from './stores/data/todos/todo-store'
+import {useObserver} from 'mobx-react-lite'
+
+// const todo = new Todo('title', userId, new TodoStore())
 
 const Test = () => {
-return (
-    <div>
-        TEST
-    </div>
-)
+    /*return useObserver(() =>
+        <div>{todo.name}</div>
+        <button onClick={() => todo.updateName('First name')}>First name</button>
+        <button onClick={() => todo.updateName('Last name')}>Last name</button>
+    )*/
 }
 
 export default Test
